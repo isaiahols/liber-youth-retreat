d4c93a3359be073207291c425a4301c05389a3d3
Isaiah Olson <isaiahols@gmail.com>
2018-10-28 11:00:22 -0600

    add Private Users

---
+++ server/Private/Admin_Users.js
@@ -0,0 +1,16 @@
+module.exports = {
+    adminUsers: () => {
+        return ([
+            {
+                email: 'isaiahols@gmail.com',
+                name: 'Isaiah',
+                adminType: 'superBoss'
+            },
+            {
+                email: 'isaiahols@pm.me',
+                name: 'Hardy',
+                adminType: 'justBoss'
+            }
+        ])
+    }
+}
